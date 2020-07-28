a937a7b00ba45d44140daa501b949eeee8fec775
haifeng.yang <haifeng.yang@renee.com>
2020-07-28 21:09:26 +0800

    代码重构

---
+++ src/main/java/tankwar/utils/IoUtils.java
@@ -0,0 +1,53 @@
+package tankwar.utils;
+
+import java.io.*;
+
+/**
+ * 读取地图类
+ */
+public class IoUtils {
+
+    public static String[] readMap(int i) {
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null; //用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
+        try {
+            String str = "";
+            StringBuilder str1 = new StringBuilder();
+            fis = new FileInputStream(new File(IoUtils.class.getClassLoader().getResource("map/level" + i +".map").getFile()));// FileInputStream
+            // 从文件系统中的某个文件中获取字节
+            isr = new InputStreamReader(fis);// InputStreamReader 是字节流通向字符流的桥梁,
+            br = new BufferedReader(isr);// 从字符输入流中读取文件中的内容,封装了一个new InputStreamReader的对象
+            while ((str = br.readLine()) != null) {
+                str1.append(str);
+            }
+            return str1.toString().split(",");
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到指定文件");
+        } catch (IOException e) {
+            System.out.println("读取文件失败");
+            return new String[0];
+        } finally {
+            try {
+                 if (br != null) {
+                    br.close();
+                }
+                if (isr != null) {
+                    isr.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+                // 关闭的时候最好按照先后顺序关闭最后开的先关闭所以先关s,再关n,最后关m
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new String[0];
+    }
+
+
+}
+
+
+
